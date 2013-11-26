4ff691b615553d367beb2cc59cf42e65ef2feb3a
wxcmonkey <wang-xc11@mails.tsinghua.edu.cn>
2013-11-26 23:18:47 +0800

    回复用户的原输入

--- ThuHelper/views.py
+++ ThuHelper/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+from xml.etree import ElementTree as ET
 
-import hashlib
+import hashlib, time
 
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
 def checkSignature(request):
     token = 'helloworld'
 
@@ -12,6 +17,14 @@ def checkSignature(request):
 
     infostr = ''.join(sorted([token, timestamp, nonce]))
     if hashlib.sha1(infostr).hexdigest() == signature:
-        return HttpResponse(echostr)
+        if request.GET.has_key('echostr'):
+            return HttpResponse(echostr)
+        else:
+            reply = '<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content></xml>'
+            xml = ET.fromstring(request.body)
+            content = xml.find("Content").text
+            fromUserName = xml.find("ToUserName").text
+            toUserName = xml.find("FromUserName").text
+            return HttpResponse(reply % (toUserName, fromUserName, str(int(time.time())), content))
     else:
         return HttpResponse("Invalid Request")
