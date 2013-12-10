225a2b687f2bee8969c609ddbfe0d747292f6888
wxcmonkey <wang-xc11@mails.tsinghua.edu.cn>
2013-12-10 20:07:21 +0800

    插入数据小后台 ..../insertmusic/

--- ThuHelper/urls.py
+++ ThuHelper/urls.py
@@ -21,6 +21,8 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'ThuHelper.views.entry', name='entry'),
     url(r'^library/', 'ThuHelper.views.library', name='library'),
+    url(r'^dbinit/', dbinit),
+    url(r'^insertmusic/', 'ThuHelper.views.insertmusic', name='insertmusic'),
 
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.STATICFILES_DIRS[0],
